fef33998269752c648abcb823ae4c60fce801fb2
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-10-03 03:30:44 +0900

    [Add] MainActivity 에서 viewModel, binding 객체 생성

--- app/src/main/java/com/example/mvvm2/MainActivity.kt
+++ app/src/main/java/com/example/mvvm2/MainActivity.kt
@@ -2,17 +2,37 @@ package com.example.mvvm2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-/**
- * api doc = https://developers.naver.com/docs/search/movie/ \
- * **/
+import android.view.View
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvm2.databinding.ActivityMainBinding
+import com.example.mvvm2.viewmodel.MovieViewModel
+import com.example.mvvm2.viewmodel.MovieViewModelIpl
 
 class MainActivity : AppCompatActivity() {
-    private val clientId: String = "KPs1Fz7hGzcZRPFvViN7"
-    private val clientSecret: String = "qm06FtUOJ8"
+
+    private lateinit var movieAdapter : RecyclerAdapter
+    private lateinit var movieViewModel: MovieViewModel
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ViewModel 객체 생성
+        movieViewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory())
+            .get(MovieViewModel::class.java)
+        movieAdapter = RecyclerAdapter()
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
+        binding.rvMovie.adapter = movieAdapter
+
     }
 }
